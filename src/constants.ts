cfa61287cc5184a7d93762e6bf7e1a5fd6a1c26a
Rodrigo Burgos <76675806+burgossrodrigo@users.noreply.github.com>
2021-12-07 00:22:17 -0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,8 @@ import JSBI from 'jsbi'
 export type BigintIsh = JSBI | bigint | string
 
 export enum ChainId {
-  MAINNET = 56,
-  TESTNET = 97
+  MAINNET = 128,
+  TESTNET = 256
 }
 
 export enum TradeType {
